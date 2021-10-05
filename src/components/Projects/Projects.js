5fe954df73d4a45e49edb9ccc8cd87eccb2edafc
Korbin Sargent <korbinksargent@gmail.com>
2021-10-05 13:24:46 -0600

    Updating projects with tech-blog

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,11 @@ const Projects = () => {
       <h1 className="title">My Favorite Projects</h1>
       <section className="container-fluid d-flex justify-content-center">
         <card class="cbBody" style={{ width: "18rem" }}>
-          <Card.Img class="samesize" variant="top" src={"/images/GitOutside"} />
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={"/images/GitOutside.png"}
+          />
           <Card.Body>
             <Card.Title class="projecttitle">GitOutside</Card.Title>
             <Button
@@ -28,7 +32,11 @@ const Projects = () => {
           </Button>
         </card>
         <card class="cbBody" style={{ width: "18rem" }}>
-          <Card.Img class="samesize" variant="top" src={"/images/GitOutside"} />
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={"/images/Dine-in.png"}
+          />
           <Card.Body>
             <Card.Title class="projecttitle">Dine-In</Card.Title>
             <Button
@@ -41,6 +49,26 @@ const Projects = () => {
           </Card.Body>
           <Button
             href="https://github.com/Korbin-Sargent/Dine-In"
+            target="_blank"
+            variant="outline-primary"
+          >
+            GitHub Link
+          </Button>
+        </card>
+        <card class="cbBody" style={{ width: "18rem" }}>
+          <Card.Img class="samesize" variant="top" src={"/images/GitOutside"} />
+          <Card.Body>
+            <Card.Title class="projecttitle">Tech Blog</Card.Title>
+            <Button
+              href="https://mighty-everglades-76044.herokuapp.com/"
+              target="_blank"
+              variant="outline-primary"
+            >
+              Deployed Link
+            </Button>
+          </Card.Body>
+          <Button
+            href="https://github.com/Korbin-Sargent/Tech-Blog"
             target="_blank"
             variant="outline-primary"
           >
