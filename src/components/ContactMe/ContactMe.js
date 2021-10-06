e9ed253ea9e605ee338bfa431c62476eccaa003f
Korbin Sargent <korbinksargent@gmail.com>
2021-10-05 19:09:21 -0600

    fixed bug with importing validateEmail function to contactme form

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { validateEmail } from "../../utils/helpers";
+import { validateEmail } from "../../utils/validateEmail";
 
 const ContactMe = () => {
   //defining state with change method and default value
