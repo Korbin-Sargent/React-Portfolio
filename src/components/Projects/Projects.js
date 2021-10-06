e84d6b38517ec434a3ad1b6960ace28f2fe17baa
Korbin Sargent <korbinksargent@gmail.com>
2021-10-06 02:48:55 -0600

    Updating project card styling

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,100 +1,105 @@
 import React from "react";
 import "./Projects.css";
 
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, CardGroup } from "react-bootstrap";
 
 const Projects = () => {
   return (
     <>
       <h1 className="title">My Favorite Projects</h1>
       <section className="container-fluid d-flex justify-content-center">
-        <Card
-          bg={"dark"}
-          text={"light"}
-          class="cbbody"
-          style={{ width: "18rem" }}
-        >
-          <Card.Img
-            class="samesize"
-            variant="top"
-            src={"/images/GitOutside.png"}
-          />
-          <Card.Body>
-            <Card.Title class="projecttitle">GitOutside</Card.Title>
-            <Button
-              href="https://git-outside.herokuapp.com/"
-              target="_blank"
-              variant="outline-primary"
-            >
-              Deployed Link
-            </Button>
-            <Button
-              href="https://github.com/Korbin-Sargent/git-outside"
-              target="_blank"
-              variant="outline-primary"
-            >
-              GitHub Link
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg={"dark"}
-          text={"light"}
-          class="cbbody"
-          style={{ width: "18rem" }}
-        >
-          <Card.Img
-            class="samesize"
-            variant="top"
-            src={"/images/Dine-in.png"}
-          />
-          <Card.Body>
-            <Card.Title class="projecttitle">Dine-In</Card.Title>
-            <Button
-              href="https://korbin-sargent.github.io/Dine-In/"
-              target="_blank"
-              variant="outline-primary"
-            >
-              Deployed Link
-            </Button>
-            <Button
-              href="https://github.com/Korbin-Sargent/Dine-In"
-              target="_blank"
-              variant="outline-primary"
-            >
-              GitHub Link
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg={"dark"}
-          text={"light"}
-          class="cbbody"
-          style={{ width: "18rem" }}
-        >
-          <Card.Img
-            class="samesize"
-            variant="top"
-            src={"/images/fitness_tracker.png"}
-          />
-          <Card.Body>
-            <Card.Title class="projecttitle">Fitness Tracker</Card.Title>
-            <Button
-              href="https://quiet-depths-83393.herokuapp.com/"
-              target="_blank"
-              variant="outline-primary"
-            >
-              Deployed Link
-            </Button>
-            <Button
-              href="https://github.com/Korbin-Sargent/Fitness-Tracker"
-              target="_blank"
-              variant="outline-primary"
-            >
-              GitHub Link
-            </Button>
-          </Card.Body>
-        </Card>
+        <CardGroup>
+          <Card
+            border="light"
+            bg={"dark"}
+            text={"light"}
+            class="cbbody"
+            style={{ width: "18rem" }}
+          >
+            <Card.Img
+              class="samesize"
+              variant="top"
+              src={"/images/GitOutside.png"}
+            />
+            <Card.Body>
+              <Card.Title class="projecttitle">GitOutside</Card.Title>
+              <Button
+                href="https://git-outside.herokuapp.com/"
+                target="_blank"
+                variant="outline-primary"
+              >
+                Deployed Link
+              </Button>
+              <Button
+                href="https://github.com/Korbin-Sargent/git-outside"
+                target="_blank"
+                variant="outline-primary"
+              >
+                GitHub Link
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card
+            border="light"
+            bg={"dark"}
+            text={"light"}
+            class="cbbody"
+            style={{ width: "18rem" }}
+          >
+            <Card.Img
+              class="samesize"
+              variant="top"
+              src={"/images/Dine-in.png"}
+            />
+            <Card.Body>
+              <Card.Title class="projecttitle">Dine-In</Card.Title>
+              <Button
+                href="https://korbin-sargent.github.io/Dine-In/"
+                target="_blank"
+                variant="outline-primary"
+              >
+                Deployed Link
+              </Button>
+              <Button
+                href="https://github.com/Korbin-Sargent/Dine-In"
+                target="_blank"
+                variant="outline-primary"
+              >
+                GitHub Link
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card
+            border="light"
+            bg={"dark"}
+            text={"light"}
+            class="cbbody"
+            style={{ width: "18rem" }}
+          >
+            <Card.Img
+              class="samesize"
+              variant="top"
+              src={"/images/fitness_tracker.png"}
+            />
+            <Card.Body>
+              <Card.Title class="projecttitle">Fitness Tracker</Card.Title>
+              <Button
+                href="https://quiet-depths-83393.herokuapp.com/"
+                target="_blank"
+                variant="outline-primary"
+              >
+                Deployed Link
+              </Button>
+              <Button
+                href="https://github.com/Korbin-Sargent/Fitness-Tracker"
+                target="_blank"
+                variant="outline-primary"
+              >
+                GitHub Link
+              </Button>
+            </Card.Body>
+          </Card>
+        </CardGroup>
       </section>
     </>
   );
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,38 +1,40 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Container } from "react-bootstrap";
 
 const Resume = () => {
   return (
     <>
-      <h1>
-        <a
-          href="./images/KorbinKSargentResume.pdf"
-          className="btn btn-success active btn-lg mt-4 p-5 resumeBtn"
-          target="_blank"
-          download="Korbin Sargent Resume"
-        >
-          Resumé PDF
-        </a>
-      </h1>
-      <ListGroup variant="flush" className="text-center">
-        List of Proficiencies
-        <ListGroup.Item>HTML</ListGroup.Item>
-        <ListGroup.Item>CSS</ListGroup.Item>
-        <ListGroup.Item>Javascript</ListGroup.Item>
-        <ListGroup.Item>APIs</ListGroup.Item>
-        <ListGroup.Item>Node.js</ListGroup.Item>
-        <ListGroup.Item>OOP</ListGroup.Item>
-        <ListGroup.Item>Express,js</ListGroup.Item>
-        <ListGroup.Item>SQL</ListGroup.Item>
-        <ListGroup.Item>ORM</ListGroup.Item>
-        <ListGroup.Item>MVC</ListGroup.Item>
-        <ListGroup.Item>NoSQL</ListGroup.Item>
-        <ListGroup.Item>PWA</ListGroup.Item>
-        <ListGroup.Item>MongoDB</ListGroup.Item>
-        <ListGroup.Item>Mongo Atlas</ListGroup.Item>
-        <ListGroup.Item>React</ListGroup.Item>
-        <ListGroup.Item>MERN Stack</ListGroup.Item>
-      </ListGroup>
+      <Container>
+        <h1>
+          <a
+            href="./images/KorbinKSargentResume.pdf"
+            className="btn btn-success active btn-lg mt-4 p-5 resumeBtn"
+            target="_blank"
+            download="Korbin Sargent Resume"
+          >
+            Resumé PDF
+          </a>
+        </h1>
+        <ListGroup className="text-center">
+          List of Proficiencies
+          <ListGroup.Item>HTML</ListGroup.Item>
+          <ListGroup.Item>CSS</ListGroup.Item>
+          <ListGroup.Item>Javascript</ListGroup.Item>
+          <ListGroup.Item>APIs</ListGroup.Item>
+          <ListGroup.Item>Node.js</ListGroup.Item>
+          <ListGroup.Item>OOP</ListGroup.Item>
+          <ListGroup.Item>Express,js</ListGroup.Item>
+          <ListGroup.Item>SQL</ListGroup.Item>
+          <ListGroup.Item>ORM</ListGroup.Item>
+          <ListGroup.Item>MVC</ListGroup.Item>
+          <ListGroup.Item>NoSQL</ListGroup.Item>
+          <ListGroup.Item>PWA</ListGroup.Item>
+          <ListGroup.Item>MongoDB</ListGroup.Item>
+          <ListGroup.Item>Mongo Atlas</ListGroup.Item>
+          <ListGroup.Item>React</ListGroup.Item>
+          <ListGroup.Item>MERN Stack</ListGroup.Item>
+        </ListGroup>
+      </Container>
     </>
   );
 };
